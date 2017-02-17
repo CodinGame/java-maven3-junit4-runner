081ce7d3296f8379ed8530b2a6fdab1d9a49983e
Sylvain Fraïssé <sylvain.fraisse@codingame.com>
2017-02-17 15:56:13 +0100

    refact: Remove the unnecessary TestCase class

--- src/main/java/com/codingame/codemachine/runner/junit/JUnitTest.java
+++ src/main/java/com/codingame/codemachine/runner/junit/JUnitTest.java
@@ -1,105 +1,45 @@
 package com.codingame.codemachine.runner.junit;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.junit.internal.runners.ErrorReportingRunner;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Request;
-import org.junit.runner.Runner;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class JUnitTest {
     private static final Pattern COMMAND_PATTERN = Pattern.compile("(?<class>[^#]+)(?:#(?<method>[^#]+))?");
 
-    private final JUnitCore jUnitCore;
-    private int statusCode;
-
-    JUnitTest() {
-        jUnitCore = new JUnitCore();
-    }
-
     int run(String testcaseSpecification) {
-        TestCase testCase = findRequest(testcaseSpecification);
-        runTestCase(testCase);
-
-        return statusCode;
+        Request request = findRequest(testcaseSpecification);
+        if (request == null) {
+            System.err.println(String.format("Testcase \"%s\" not found", testcaseSpecification));
+            return 2;
+        }
+        return runTestCase(request);
     }
 
-    TestCase findRequest(String testcaseSpecification) {
-        TestCase request = null;
+    Request findRequest(String testcaseSpecification) {
+        Request request = null;
         Matcher matcher = COMMAND_PATTERN.matcher(testcaseSpecification);
         if (matcher.matches()) {
             try {
                 Class<?> clazz = Class.forName(matcher.group("class"));
                 String method = matcher.group("method");
                 if (method != null) {
-                    request = TestCase.createTestCase(Request.method(clazz, method), testcaseSpecification);
+                    request = Request.method(clazz, method);
                 }
                 else {
-                    request = TestCase.createTestCase(Request.aClass(clazz), testcaseSpecification);
+                    request = Request.aClass(clazz);
                 }
             }
-            catch (ClassNotFoundException ignored) {
-                request = TestCase.createTestCase();
-            }
+            catch (ClassNotFoundException ignored) {}
         }
         return request;
     }
 
-    private void runTestCase(TestCase testCase) {
-        if (testCase.exists()) {
-            jUnitCore.addListener(new TestResultFormatter());
-            if (testCase.run(jUnitCore)) {
-                statusCode = 0;
-            } else {
-                statusCode = 1;
-            }
-        }
-        else {
-            statusCode = 2;
-            System.err.println("Testcase not found \""+ testCase.description() + "\"");
-        }
+    private int runTestCase(Request request) {
+        JUnitCore jUnitCore = new JUnitCore();
+        jUnitCore.addListener(new TestResultFormatter());
+        return jUnitCore.run(request).wasSuccessful() ? 0 : 1;
     }
-
-    static class TestCase {
-        static TestCase createTestCase() {
-            return new TestCase(null, null);
-        }
-
-        static TestCase createTestCase(Request request, String description) {
-            TestCase testCase = new TestCase(null, null);
-            if (request != null) {
-                Runner runner = request.getRunner();
-                if (!(runner instanceof ErrorReportingRunner)) {
-                    testCase = new TestCase(request, description);
-                }
-            }
-            return testCase;
-        }
-
-        private final Request request;
-        private final String description;
-
-        private TestCase(Request request, String description) {
-            this.request = request;
-            this.description = description;
-        }
-
-        Request request() {
-            return this.request;
-        }
-
-        String description() {
-            return this.description;
-        }
-
-        boolean exists() {
-            return request() != null;
-        }
-
-        boolean run(final JUnitCore jUnitCore) {
-            return jUnitCore.run(request).wasSuccessful();
-        }
-    }
-
 }
